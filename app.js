695867d6802e73dbf0ec9f9c69ae2675512574c8
Danielpac77 <danielpac.dr@gmail.com>
2025-01-25 15:50:04 -0600

    Primera parte del trabajo realizado

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// variables a utilizar 
+
+let amigos = [];
+let nombreParticipante = document.getElementById('amigo');
+
+
+// funciones a utilizar
+
+function tomarDatos () {
+let nombre = nombreParticipante.value.trim();
+
+if (nombre == ""){
+    alert("Por favor ingrese un nombre valido")
+    return;
+}
+
+if (nombre.lenght > 20) {
+    alert("El nombre no puede tener más de 20 caracteres");
+    return;
+}
+
+
+}
+
+function registrarDatos () {}
+
+function listaDatos () {}
+
+function limpiarDatos () {}
+
+
+
+
+
